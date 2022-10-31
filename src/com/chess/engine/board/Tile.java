eede333c4726bf37412c04cecea3a2e92bff8040
Santiago Robles <gsrrobles@gmail.com>
2022-10-30 22:26:29 -0700

    Added Abstract class Piece

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -9,20 +9,20 @@ import java.util.Map;
 public abstract class Tile {
     protected final int tileCoordinate;
 
-    private static final Map<Integer, EmpyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
-    private static Map<Integer, EmpyTile> createAllPossibleEmptyTiles() {
-        final Map<Integer, EmpyTile> emptyTileMap = new HashMap<>();
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
 
         for (int i = 0; i < 64; i++) {
-            emptyTileMap.put(i, new EmpyTile(i));
+            emptyTileMap.put(i, new EmptyTile(i));
         }
 
         return ImmutableMap.copyOf(emptyTileMap);
     }
 
     public static Tile createTile(final int tileCoordinate, final Piece piece){
-        return piece != null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece != null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
     private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
@@ -32,8 +32,8 @@ public abstract class Tile {
 
     public abstract Piece getPiece();
 
-    public static final class EmpyTile extends Tile {
-        private EmpyTile(final int tileCoordinate) {
+    public static final class EmptyTile extends Tile {
+        private EmptyTile(final int tileCoordinate) {
             super(tileCoordinate);
         }
 
