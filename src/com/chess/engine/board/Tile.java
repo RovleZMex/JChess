d585f6be4fbd7d853f71ea4cf3e32a0e160a2fcc
Santiago <48573143+RovleZMex@users.noreply.github.com>
2022-10-31 11:43:33 -0700

    Added the Move class code

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -14,7 +14,7 @@ public abstract class Tile {
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
 
-        for (int i = 0; i < 64; i++) {
+        for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
             emptyTileMap.put(i, new EmptyTile(i));
         }
 
@@ -52,7 +52,7 @@ public abstract class Tile {
 
         private final Piece pieceOnTile;
 
-        private OccupiedTile(int tileCoordinate, Piece piece) {
+        private OccupiedTile(int tileCoordinate, final Piece piece) {
             super(tileCoordinate);
             this.pieceOnTile = piece;
         }
