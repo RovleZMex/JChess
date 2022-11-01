567d5e0043e667104d76098316dd8ab85debeeb0
Santiago <48573143+RovleZMex@users.noreply.github.com>
2022-11-01 11:34:44 -0700

    Added board behavior

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -17,7 +17,9 @@ public abstract class Piece {
         //TODO more work here!!
         this.isFirstMove = false;
     }
-
+    public int getPiecePosition(){
+        return this.piecePosition;
+    }
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
     }
